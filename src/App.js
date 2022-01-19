3d5bfac7f4801b9811cf84947d814f1cacef6105
Andre William <andrecool.cool@gmail.com>
2022-01-19 14:16:21 +0700

    change basename

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //* React
 import * as React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 //* pages and components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -12,49 +12,46 @@ import './styles/tailwind.css';
 import './styles/custom.css';
 //* animate
 //animate
-import Aos from "aos";
-import "aos/dist/aos.css";
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
-
   let [isBlurred, setIsBlurred] = React.useState(false);
   let [isOpen, setIsOpen] = React.useState(false);
 
   let toggleNav = (e) => {
     e.stopPropagation();
-    setIsOpen(state => !state);
-  }
+    setIsOpen((state) => !state);
+  };
 
   React.useEffect(() => {
-    Aos.init({duration: 1000});
+    Aos.init({ duration: 1000 });
     Aos.refresh();
-  },[]);
+  }, []);
 
   let handleScroll = () => {
-    if(window.scrollY > 20)
-      setIsBlurred(true);
-    else
-      setIsBlurred(false);
-  }
+    if (window.scrollY > 20) setIsBlurred(true);
+    else setIsBlurred(false);
+  };
 
   window.addEventListener('scroll', handleScroll);
-  window.addEventListener('click', (e) =>{
-    if( isOpen && e.target.id !== 'modal'){
+  window.addEventListener('click', (e) => {
+    if (isOpen && e.target.id !== 'modal') {
       setIsOpen(false);
     }
-  })
+  });
 
   return (
-    <Router>
-        <div className="font-sans" id="home">
-          <Navbar isBlurred={isBlurred} toggleNav={toggleNav} isOpen={isOpen} />
-          <Dropdown isOpen={isOpen} toggleNav={toggleNav}/>
-          <Switch>
-              <Route to="/" exact component={Home}/>
-          </Switch>
-          <Footer/>
-          <Particle/>
-        </div>
+    <Router basename={window.location.pathname || ''}>
+      <div className="font-sans" id="home">
+        <Navbar isBlurred={isBlurred} toggleNav={toggleNav} isOpen={isOpen} />
+        <Dropdown isOpen={isOpen} toggleNav={toggleNav} />
+        <Switch>
+          <Route to="/" exact component={Home} />
+        </Switch>
+        <Footer />
+        <Particle />
+      </div>
     </Router>
   );
 }
